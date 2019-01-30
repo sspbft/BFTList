952d5b1021bc934e9a9b1cffe7ffd9ce0f0a6d33
TheresePetersson <theresemariapetersson@gmail.com>
2019-01-30 14:33:52 +0200

    Shell for Primary Monitoring, Enums for PM and VE modules, updated tests accordingly

---
+++ modules/enums.py
@@ -0,0 +1,18 @@
+"""Enums related to the modules of the algorithm."""
+from enum import Enum
+
+
+class PrimaryMonitoringEnums(Enum):
+    """Represents a string for v_status."""
+
+    OK = 1
+    NO_SERVICE = 2
+    V_CHANGE = 3
+
+
+class ViewEstablishmentEnums(Enum):
+    """Represents strings for return values from automaton."""
+
+    NO_ACTION = 1
+    RESET = 2
+    NO_RETURN_VALUE = 3
