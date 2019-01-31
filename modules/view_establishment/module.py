edacfc828a087e0473088c8c0bb786e7332eec8c
TheresePetersson <theresemariapetersson@gmail.com>
2019-01-31 15:12:18 +0200

    Macros and interface function implemented View Establishment module

--- modules/view_establishment/module.py
+++ modules/view_establishment/module.py
@@ -8,61 +8,105 @@ import time
 class ViewEstablishmentModule(AlgorithmModule):
     """Models the View Establishment module."""
 
+    VIEWS = "views"
+    PHASE = "phase"
+    WITNESSES = "witnesses"
+
     phs = []
     witnesses = []
-    witnessesSet = {}
+    witnesses_set = set()
     echo = []
     pred_and_action = None
+    resolver = None
+    number_of_nodes = 0
+    number_of_byzantine = 0
+    id = 0
 
-    def __init__(self, resolver, n=2):
+    def __init__(self, resolver, id=0, n=2, byz=0):
         """Initializes the module."""
         self.resolver = resolver
-        self.view = 0
-        self.phs = [0] * n
-        self.witness = [False] * n
-        self.echo = [None] * n
+        self.phs = [0 for i in range(n)]
+        self.witnesses = [False for i in range(n)]
+        self.echo = [
+            {self.VIEWS: None, self.PHASE: None, self.WITNESSES: None}
+            for i in range(n)
+        ]
         self.pred_and_action = PredicatesAndAction(self, self.resolver, n)
+        self.number_of_nodes = n
+        self.id = id
+        self.number_of_byzantine = byz
 
     def run(self):
         """Called whenever the module is launched in a separate thread."""
         while True:
-            # print(__name__ + ": running")
             time.sleep(3)
 
-    # Macros algorithm 1
-    def echo_no_witn(self, node_k):
+    # Macros
+    def echo_no_witn(self, processor_k):
         """Method description.
 
-        Checks if node k has reported(echo) a view and phase matching
+        Checks if processor k has reported(echo) a view and phase matching
         the current view and phase.
         """
-        raise NotImplementedError
+        return (self.get_view(self.id) ==
+                self.echo[processor_k].get(self.VIEWS) and
+                self.phs[self.id] == self.echo[processor_k].get(self.PHASE))
 
     def witnes_seen(self):
-        """True if witnessSet (including current node) is greater than 4f."""
-        raise NotImplementedError
+        """Method description.
+
+        True if witnessSet (including current node) is greater than 4f and
+        processor i has been witnessed.
+        """
+        if(self.witnesses[self.id]):
+            processor_set = set()
+            for processor_id in self.witnesses_set:
+                if(self.echo[self.id] == self.echo[processor_id]):
+                    processor_set.add(processor_id)
+            processor_set.union({self.id})
+            return (len(processor_set) >= (4 * self.number_of_byzantine + 1))
+        return False
 
     def next_phs(self):
         """Proceeds the phase from 0 to 1, or 1 to 0."""
-        raise NotImplementedError
+        self.phs[self.id] ^= 1
 
     # Interface functions
-
-    def get_phs(node_k):
+    def get_phs(self, processor_k):
         """Returns the phase of node k according to current node."""
-        raise NotImplementedError
+        return self.phs[processor_k]
 
     def init_module(self):
         """Use to reset the module."""
-        raise NotImplementedError
+        self.phs = [0 for i in range(self.number_of_nodes)]
+        self.witnesses = [False for i in range(self.number_of_nodes)]
+        self.witnesses_set = set()
 
-    # TODO Change this to direct communication with Algorithm 2
-    # , remove default values
-    # Methods to communicate with Algorithm 2 (still View Establishment Module)
-    def get_view(self, node_k=0):
+    # Methods to communicate with Algorithm 2 (View Establishment Module)
+    def get_view(self, processor_k):
         """Calls get_view of PredicatesAndAction."""
-        return self.pred_and_action.get_view(node_k)
+        return self.pred_and_action.get_view(processor_k)
 
     def allow_service(self):
         """Calls allow_service of PredicatesAndAction."""
         return self.pred_and_action.allow_service()
+
+    # Methods to communicate with other processors
+    def send_msg(self, processor_j):
+        """Method description.
+
+        Calls the Resolver to send a message containing the phase, view and
+        witnesses of processor i and what processor wants to echo about
+        processor j to processor_j
+        """
+        raise NotImplementedError
+
+    def receive_msg(self, msg):
+        """Method description.
+
+        Resolver calls this function when a message to the View Establishment
+        module from another processor has been delievered.
+        Valids the message and updates phase, witnesses, echo and views for the
+        sending processor.
+        """
+        raise NotImplementedError
--- tests/view_establishment_test.py
+++ tests/view_establishment_test.py
@@ -7,6 +7,81 @@ from resolve.enums import Function, Module
 
 
 class ViewEstablishmentModuleTest(unittest.TestCase):
+
+    # Macros
+
+    def test_echo_no_witn(self):
+        resolver = Resolver()
+        view_est_mod = ViewEstablishmentModule(resolver)
+
+        # Both conditions are fulfilled
+        view_est_mod.phs[view_est_mod.id] = 0
+        view_est_mod.get_view = MagicMock(return_value = {"current": 0, "next": 1})
+        view_est_mod.echo[1] = {view_est_mod.VIEWS: {"current": 0, "next": 1}, view_est_mod.PHASE: 0, view_est_mod.WITNESSES: None}
+        self.assertTrue(view_est_mod.echo_no_witn(1))
+
+        # The view in the echo is not correct
+        view_est_mod.echo[1] = {view_est_mod.VIEWS: {"current": 0, "next": 0}, view_est_mod.PHASE: 0, view_est_mod.WITNESSES: None}
+        self.assertFalse(view_est_mod.echo_no_witn(1))
+
+        # The phase in the echo is not correct
+        view_est_mod.echo[1] = {view_est_mod.VIEWS: {"current": 0, "next": 1}, view_est_mod.PHASE: 1, view_est_mod.WITNESSES: None}
+        self.assertFalse(view_est_mod.echo_no_witn(1))
+
+    def test_witnes_seen(self):
+        resolver = Resolver()
+        view_est_mod = ViewEstablishmentModule(resolver)
+
+        # Both condition fulfilled with f = 0
+        view_est_mod.witnesses[view_est_mod.id] = True
+        view_est_mod.witnesses_set = {1}
+        view_est_mod.echo[0] = {view_est_mod.VIEWS: {"current": 0, "next": 1}, view_est_mod.PHASE: 1, view_est_mod.WITNESSES: None}
+        view_est_mod.echo[1] = {view_est_mod.VIEWS: {"current": 0, "next": 1}, view_est_mod.PHASE: 1, view_est_mod.WITNESSES: None}
+        self.assertTrue(view_est_mod.witnes_seen())
+
+        # Processor i has not been witnessed
+        view_est_mod.witnesses[view_est_mod.id] = False
+        self.assertFalse(view_est_mod.witnes_seen())
+
+        # f = 1, meaning the set is not big enough, set = 2 and 5 is needed
+        view_est_mod.number_of_byzantine = 1
+        view_est_mod.witnesses[view_est_mod.id] = True
+        self.assertFalse(view_est_mod.witnes_seen())
+
+
+    def test_next_phs(self):
+        resolver = Resolver()
+        view_est_mod = ViewEstablishmentModule(resolver)
+        view_est_mod.phs[view_est_mod.id] = 0
+
+        # Move to phase 1
+        view_est_mod.next_phs()
+        self.assertEqual(view_est_mod.phs[view_est_mod.id], 1)
+
+        # Move to phase 2
+        view_est_mod.next_phs()
+        self.assertEqual(view_est_mod.phs[view_est_mod.id], 0)
+
+    # Interface functions
+
+    def test_get_phs(self):
+        resolver = Resolver()
+        view_est_mod = ViewEstablishmentModule(resolver)
+        view_est_mod.phs = [0, 1]
+        self.assertEqual(view_est_mod.get_phs(0), 0)
+        self.assertEqual(view_est_mod.get_phs(1), 1)
+
+    def test_init(self):
+        resolver = Resolver()
+        view_est_mod = ViewEstablishmentModule(resolver)
+        view_est_mod.phs = [0, 1]
+        view_est_mod.witnesses_set = {0}
+        view_est_mod.witnesses = [True, True]
+
+        view_est_mod.init_module()
+        self.assertEqual(view_est_mod.phs, [0, 0])
+        self.assertEqual(view_est_mod.witnesses_set, set())
+        self.assertEqual(view_est_mod.witnesses, [False, False])
     
     # Function added for re-routing inter-module communication
     def test_get_view_from_predicts_and_action(self):
