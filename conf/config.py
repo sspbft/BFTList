844ac3ba5424d11b149d2a5adb3ad38c7919d4d8
Axel Niklasson <axel.niklasson@live.com>
2019-02-04 13:35:19 +0200

    Integration tests test run on Travis

--- conf/config.py
+++ conf/config.py
@@ -1,9 +1,16 @@
 """Module handling config related actions."""
 from communication.node import Node
+import os
 
 
 def get_nodes(hosts_path="conf/hosts.txt"):
-    """Parses nodes file to a dict of nodes such that dct[id] = node."""
+    """Parses nodes file to a dict of nodes such that dct[id] = node.
+
+    Can be overridden by specifying the environment variable HOSTS_PATH, which
+    corresponds to the absolute path to the desired hosts file.
+    """
+    if os.getenv("HOSTS_PATH"):
+        hosts_path = os.getenv("HOSTS_PATH")
     with open(hosts_path) as f:
         lines = [x.strip().split(",") for x in f.readlines()]
         nodes = {}
--- tests/integration_tests/helpers.py
+++ tests/integration_tests/helpers.py
@@ -1,6 +1,7 @@
 import asyncio
 import os
 import requests
+import psutil
 from requests.packages.urllib3.util.retry import Retry
 from requests.adapters import HTTPAdapter
 import conf.config
@@ -10,6 +11,7 @@ HOST = "http://localhost"
 BASE_PORT = 4000
 N = 5
 F = 1
+RELATIVE_PATH_FIXTURES_HOST = "./tests/integration_tests/fixtures/nodes.txt"
 
 def run_coro(coro):
     """Runs a co-routing in the default event loop and retuns it result."""
@@ -17,7 +19,7 @@ def run_coro(coro):
 
 def get_nodes():
     """Helper method to get fixed set of nodes for integration tests."""
-    return conf.config.get_nodes("./tests/integration_tests/fixtures/nodes.txt")
+    return conf.config.get_nodes(RELATIVE_PATH_FIXTURES_HOST)
 
 # HTTP related helpers
 def session():
@@ -34,18 +36,30 @@ async def GET(node_id, path):
         raise ValueError(f"Got bad response {r.status_code} from url {url}.")
     return {"status_code": r.status_code, "data": r.json()}
 
+
+# application runner helpers
 async def launch_bftlist():
     nodes = get_nodes()
     cmd = "source env/bin/activate && python3.7 main.py"
     cwd = os.path.abspath(".")
+    pids = []
+
     for node_id in nodes.keys():
         env = os.environ.copy()
         env["ID"] = str(node_id)
         env["API_PORT"] = str(4000 + node_id)
-        env["NUMBER_OF_NODES"] = N
-        env["NUMBER_OF_BYZANTINE"] = F
+        env["NUMBER_OF_NODES"] = str(N)
+        env["NUMBER_OF_BYZANTINE"] = str(F)
+        env["HOSTS_PATH"] = os.path.abspath(RELATIVE_PATH_FIXTURES_HOST)
         p = subprocess.Popen(cmd, shell=True, cwd=cwd, env=env)
-        return p.pid
+        pids.append(p.pid)
 
-def kill(process):
-    pass
+    await asyncio.sleep(2) # give nodes time to start before returning
+    return pids
+
+def kill(pids):
+    for pid in pids:
+        process = psutil.Process(pid)
+        for proc in process.children(recursive=True):
+            proc.kill()
+        process.kill()
