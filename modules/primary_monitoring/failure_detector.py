48ed71d8619c16b6b6ce892975eb2d17a645ceed
TheresePetersson <theresemariapetersson@gmail.com>
2019-02-22 15:04:07 +0200

    Macro, interface and some help methods with unit testing

--- modules/primary_monitoring/failure_detector.py
+++ modules/primary_monitoring/failure_detector.py
@@ -1,33 +1,88 @@
 """Contains code related to the Primary Monitoring module."""
 
 import time
-# from resolve.enums import Function, Module
+from resolve.enums import Function, Module
 # from modules.enums import PrimaryMonitoringEnums
 
 
-class PrimaryMonitoringModule:
+class FailureDetectorModule:
     """Models the Primary Monitoring moduel - Failure detector algorithm."""
 
-    def __init__(self, resolver, ):
+    def __init__(self, id, resolver, n, f):
         """Initializes the module."""
         self.resolver = resolver
+        self.id = id
+        self.number_of_nodes = n
+        self.number_of_byzantine = f
+        self.beat = [0 for i in range(n)]
+        self.cnt = 0
+        self.prim_susp = [False for i in range(n)]
+        self.cur_check_req = []
 
     def run(self):
         """Called whenever the module is launched in a separate thread."""
+        # TODO change while to upon token.
         while True:
             time.sleep(1)
+
+        # After line 14, meaning if j == prim:
+            # Call self.check_progress_by_prim(j)
+
+        # else:
+            # continue here
 
     # Macros
     def reset(self):
         """Resets local variables."""
-        raise NotImplementedError
+        self.beat = [0 for i in range(self.number_of_nodes)]
+        self.cnt = 0
+        self.prim_susp = [False for i in range(self.number_of_nodes)]
+        self.cur_check_req = []
 
     # Interface functions
     def suspected(self):
         """Returns true if the processor suspects the primary to be faulty."""
-        raise NotImplementedError
+        num_of_processor = 0
+
+        for processor_id in range(self.number_of_nodes):
+            if (self.get_current_view(processor_id) ==
+                    self.get_current_view(self.id) and
+               self.prim_susp[processor_id]):
+                num_of_processor += 1
+        if num_of_processor >= (3 * self.number_of_byzantine + 1):
+            return True
+        return False
 
     # Functions added for inter-module communication
     def get_current_view(self, processor_id):
         """Calls get_current_view method at View Establishment module."""
-        raise NotImplementedError
+        return self.resolver.execute(Module.VIEW_ESTABLISHMENT_MODULE,
+                                     Function.GET_CURRENT_VIEW, processor_id)
+
+    def get_pend_reqs(self):
+        """Calls get_pend_reqs in Replication module"""
+        return self.resolver.execute(Module.REPLICATION_MODULE,
+                                     Function.GET_PEND_REQS)
+
+    # Added functions
+    def check_progress_by_prim(self, prim):
+        """Checks for progress done by the primary.
+
+        Line 15-17
+        """
+        # Check progress
+        exist_progress = False
+        if len(self.cur_check_req) == 0:
+            exist_progress = True
+        else:
+            for req in self.cur_check_req:
+                if req not in self.get_pend_reqs():
+                    exist_progress = True
+                    break
+        # If there has been progress, reset the cnt
+        if exist_progress:
+            self.cnt = 0
+            self.cur_check_req = self.get_pend_reqs()
+        # The primary has not made progress, increase our own counter
+        else:
+            self.cnt += 1
---
+++ tests/unit_tests/test_failure_detector.py
@@ -0,0 +1,94 @@
+import unittest
+import os
+from conf import config
+
+
+from unittest.mock import Mock, MagicMock
+from resolve.resolver import Resolver
+from resolve.enums import Function, Module
+from modules.replication.models.client_request import ClientRequest
+from modules.replication.models.operation import Operation
+from modules.enums import OperationEnums
+from modules.primary_monitoring.failure_detector import FailureDetectorModule
+
+class TestConfig(unittest.TestCase):
+
+    def setUp(self):
+        self.resolver = Resolver()
+        self.clientRequest1 = ClientRequest(1, 1, Operation(OperationEnums.APPEND, 1))
+        self.clientRequest2 = ClientRequest(2, 2, Operation(OperationEnums.APPEND, 1))
+
+    def test_call_view_establishment_module(self):
+        fail_det = FailureDetectorModule(0, self.resolver, 6, 1)
+        fail_det.resolver.execute = MagicMock(return_value = 0)
+
+        self.assertEqual(fail_det.get_current_view(0),0)
+        fail_det.resolver.execute.assert_called_once_with(
+            Module.VIEW_ESTABLISHMENT_MODULE,
+            Function.GET_CURRENT_VIEW,
+            0
+        )
+
+    def test_call_replication_module(self):
+        fail_det = FailureDetectorModule(0, self.resolver, 6, 1)
+        fail_det.resolver.execute = MagicMock(return_value = [])
+
+        self.assertEqual(fail_det.get_pend_reqs(),[])
+        fail_det.resolver.execute.assert_called_once_with(
+            Module.REPLICATION_MODULE,
+            Function.GET_PEND_REQS
+        )
+
+    def test_reset(self):
+        fail_det = FailureDetectorModule(0, self.resolver, 6, 1)
+        # Change the values to non-default once
+        fail_det.beat = [1 for i in range(6)]
+        fail_det.cnt = 2
+        fail_det.prim_susp = [True for i in range(6)]
+        fail_det.cur_check_req = [ClientRequest(0, None, None)]
+
+        fail_det.reset()
+        self.assertEqual(fail_det.beat, [0 for i in range(6)])
+        self.assertEqual(fail_det.cnt, 0)
+        self.assertEqual(fail_det.prim_susp, [False for i in range(6)])
+        self.assertEqual(fail_det.cur_check_req, [])
+
+
+    def test_suspected(self):
+        fail_det = FailureDetectorModule(0, self.resolver, 6, 1)
+        # All has the same view and their primSusp is False
+        fail_det.get_current_view = MagicMock(return_value = 0)
+        self.assertFalse(fail_det.suspected())
+        
+        # Same view but primSusp is True for 3f+1 of them
+        fail_det.prim_susp = [True, True, False, True, False, True]
+        self.assertTrue(fail_det.suspected())
+
+        # Now they are not in same view
+        fail_det.prim_susp = [True for i in range(6)]
+        fail_det.get_current_view = MagicMock(side_effect = lambda x: x%2)
+        # just double checking the re-assignment of prim_susp
+        self.assertTrue(fail_det.prim_susp[2])
+        self.assertFalse(fail_det.suspected())
+
+    def test_check_progress_by_prim(self):
+        fail_det = FailureDetectorModule(0, self.resolver, 6, 1)
+        fail_det.resolver.execute = MagicMock(return_value = [self.clientRequest1])
+        fail_det.cur_check_req = [self.clientRequest1, self.clientRequest2]
+
+        # let cnt not be default value
+        fail_det.cnt = 2
+        # There has been progress since clientRequest2 is missing in get_pend_reqs
+        fail_det.check_progress_by_prim(1)
+        self.assertEqual(fail_det.cur_check_req, [self.clientRequest1])
+        self.assertEqual(fail_det.cnt, 0)
+
+        # There has been no progress, cnt should be incremented
+        fail_det.cur_check_req = [self.clientRequest1]        
+        fail_det.check_progress_by_prim(1)
+        self.assertEqual(fail_det.cnt, 1)
+
+        # cur_check_req is empty, cnt should be reset
+        fail_det.cur_check_req = []        
+        fail_det.check_progress_by_prim(1)
+        self.assertEqual(fail_det.cnt, 0)
