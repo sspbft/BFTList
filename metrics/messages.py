edb54db82726d91a8824b72a45cee6024b61a526
Axel Niklasson <axel.niklasson@live.com>
2019-03-08 13:53:58 +0200

    use histogram instead

--- metrics/messages.py
+++ metrics/messages.py
@@ -1,6 +1,6 @@
 """Metrics related to messages."""
 
-from prometheus_client import Counter, Gauge, Summary
+from prometheus_client import Counter, Gauge, Histogram
 
 msgs_sent = Counter("msg_sent",
                     "Number of messages sent between nodes",
@@ -11,10 +11,10 @@ msg_rtt = Gauge("msg_rtt",
                 "Time taken to send a message to a node and get an ACK",
                 ["node_id", "receiver_id", "receiver_hostname"])
 
-msg_latency = Summary("msg_latency",
-                      "Time between sending a message and getting ACK",
-                      "node_id", "receiver_id", "receiver_hostname"
-                      )
+msg_latency = Histogram("msg_latency",
+                        "Time between sending a message and getting ACK",
+                        "node_id", "receiver_id", "receiver_hostname"
+                        )
 
 msgs_in_queue = Gauge("msgs_in_queue",
                       "The amount of messages waiting to be sent over channel",
