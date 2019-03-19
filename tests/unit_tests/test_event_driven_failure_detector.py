a468bb308374f033edcb5633459dd2c22c8f57ab
Axel Niklasson <axel.niklasson@live.com>
2019-03-19 18:49:13 +0200

    Update test

--- tests/unit_tests/test_event_driven_failure_detector.py
+++ tests/unit_tests/test_event_driven_failure_detector.py
@@ -41,14 +41,15 @@ class TestEventDrivenFailureDetector(unittest.TestCase):
     def test_on_msg_recv(self):
         self.module.send_token = MagicMock()
 
-        # counter for sender should be incremented when sending current token
+        # counter for sender should be incremented when receiving current token
         self.assertEqual(self.module.counters[3], 0)
         msg = self.build_msg(3, 0, 0)
         self.module.on_msg_recv(msg)
         self.assertEqual(self.module.counters[3], 1)
+        # should send token back to sender
         self.assertEqual(self.module.send_token.call_count, 1)
 
-        # counter should stay the same when sending invalid token
+        # counter for sender should stay the same when sending invalid token
         self.assertEqual(self.module.counters[3], 1)
         msg = self.build_msg(3, -1, 0)
         self.module.on_msg_recv(msg)
@@ -61,6 +62,7 @@ class TestEventDrivenFailureDetector(unittest.TestCase):
         msg = self.build_msg(3, 0, 1)
         self.module.on_msg_recv(msg)
         self.assertEqual(old_counters, self.module.counters)
+        # token should be sent back
         self.assertEqual(self.module.send_token.call_count, 2)
     
     def test_send_token(self):
@@ -73,16 +75,17 @@ class TestEventDrivenFailureDetector(unittest.TestCase):
         # should return [] for 0 correct processors
         self.assertEqual(self.module.get_correct_processors(), [])
 
-        # inject two correct processors, should return their IDs
+        # inject two correct processors and one faulty, get_correct_processors should return the correct IDs
         self.module.counters[0] = K
         self.module.counters[2] = K + 1
+        self.module.counters[3] = K - 1
         self.assertCountEqual(self.module.get_correct_processors(), [0,2])
     
     def test_correct_processors_have_replied(self):
         # should be False since no processors have yet replied
         self.assertFalse(self.module.correct_processors_have_replied())
 
-        # force n-2f processors to have acked K times, should now be true
+        # force n-2f processors to have acked K times, should now return true
         self.module.counters = {i: K for i in range(N - 2*F)}
         self.assertTrue(self.module.correct_processors_have_replied())
     
