fbb99260b614386f2f3679efacd1095525c393cd
TheresePetersson <32383231+TheresePetersson@users.noreply.github.com>
2019-03-19 18:44:46 +0200

    Feature/interface pm (#97)
    * Interface function
    
    * Don't ping localhost
    
    * Try/except in parsing ping output

--- metrics/latency_monitor.py
+++ metrics/latency_monitor.py
@@ -1,22 +1,38 @@
 """Module containing a latency monitor used for latency metrics."""
+
+# standard
 import subprocess
 import os
+import logging
 
+# local
 from conf.config import get_nodes
 from metrics.latency import host_latency
+
+# globals
+logger = logging.getLogger(__name__)
 
 
 def monitor_node_latencies():
     """Continously emits latency metric for other nodes by pinging them."""
     ID = int(os.getenv("ID"))
     nodes = get_nodes()
-    other_nodes = {k: nodes[k] for k in nodes if k != ID}
+    other_nodes = {k: nodes[k] for k in nodes if k != ID and
+                   nodes[k].hostname != "localhost"}
+
+    if len(other_nodes) == 0:
+        logger.info(f"No use to ping when running locally, aborting")
+        return
 
     while True:
         for n_id in other_nodes:
             node = nodes[n_id]
-            cmd = f"sudo sh ./metrics/ping.sh {node.hostname}".split(" ")
-            res = subprocess.Popen(cmd, stdout=subprocess.PIPE).communicate()
-            latency = float(res[0].decode().replace("\n", ""))
-            host_latency.labels(ID, nodes[ID].hostname, n_id,
-                                node.hostname).set(latency)
+            try:
+                cmd = f"sudo sh ./metrics/ping.sh {node.hostname}".split(" ")
+                res = subprocess.Popen(cmd,
+                                       stdout=subprocess.PIPE).communicate()
+                latency = float(res[0].decode().replace("\n", ""))
+                host_latency.labels(ID, nodes[ID].hostname, n_id,
+                                    node.hostname).set(latency)
+            except Exception as e:
+                logger.error(f"Got error {e} when pinging {node.hostname}")
